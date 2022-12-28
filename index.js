e8a6a7c3982b452a83ee299e8670e61a143de377
Atif1727 <ghoghaatif2002@gmail.com>
2022-12-28 11:35:12 +0530

    upload file api

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 const express = require('express');
-require("./config");
-const Product = require('./product');
+const multer=require('multer');
+
 const app = express();
 
-app.use(express.json());
-app.post("/create", async (req, resp) => {
-    let data = new Product(req.body);
-    const result = await data.save();
-    resp.send(result);
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'uploads');//cb means callback
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + "-" + Date.now() + ".jpg")
+        }
+    })
+}).single('file_name');
+
+app.post("/upload", upload, (req, resp) => {
+    resp.send("file upload")
 });
 
-app.get("/list", async (req, resp) => {
-    let data = await Product.find();
-    resp.send(data);
-})
-
-app.delete("/delete/:_id", async (req, resp) => {
-    console.log(req.params)
-    let data = await Product.deleteOne(req.params);
-    resp.send(data);
-})
-
-
-app.put("/update/:_id",async (req, resp) => {
-    console.log(req.params)
-    let data = await Product.updateOne(
-        req.params,
-        {$set: req.body}
-    );
-    resp.send(data);
-})
-
-app.listen(5000)
+app.listen(6000);
